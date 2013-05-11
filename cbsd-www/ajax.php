8e7043bfc3ba365d461fa7fc5d39df95df089e07
Oleg Ginzburg <olevole@olevole.ru>
2013-05-11 13:52:46 +0400

    wip: scenes re-work

--- cbsd-www/ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if($_SERVER['REQUEST_METHOD']=='POST')
-{
-	echo json_encode(array('msg'=>'ok!'));
-}
--- cbsd-www/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require('includes/main.php');
-redirect_url('status.php');
-
-?>
