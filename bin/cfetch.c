0530168c41838a35b6f2067a244e2abdb0ed6841
olevole <olevole@olevole.ru>
2017-07-14 01:26:44 +0300

    Automatic selection of a suitable mirror when downloading images; Fixes for grep error on initial setup.  Issue #176, Thanks to Vladislav V. Prodan aka click0; Correct text on initenv stage to avoid confusing the user with the version mismatch. Issue #177, Thanks to Vladislav V. Prodan aka click0;

--- bin/cfetch.c
+++ bin/cfetch.c
@@ -15,14 +15,21 @@
 #include <string.h>
 #include <errno.h>
 #include <fetch.h>
+#include <signal.h> // sigaction(), sigsuspend(), sig*()
 #include <unistd.h>
 
 static int	fetch_files(char *, char *);
+void		handle_signal(int);
+void		handle_sigalrm(int);
+
+off_t		current_bytes=0;
+int		speedtest=0;
 
 int usage()
 {
 	printf("fetch remove file via http[s]\n");
 	printf("require: -u url -o out\n\n");
+	printf("optional: -s <only print s url -o out\n\n");
 	printf("Example: cbsd cfetch -u url -o /tmp/out\n");
 	exit(0);
 }
@@ -31,13 +38,30 @@ int
 main(int argc, char *argv[])
 {
 	int		c;
-	char           *url = NULL, *fout = NULL;
+	char		*url = NULL, *fout = NULL;
+
+	if(argc<2) usage();
 
 	if (!strcmp(argv[1], "--help"))
 		usage();
 
+	struct sigaction sa;
+	// Setup the sighub handler
+	sa.sa_handler = &handle_signal;
+
+	// Restart the system call, if at all possible
+	sa.sa_flags = SA_RESTART;
+
+	// Block every signal during the handler
+	sigfillset(&sa.sa_mask);
+
+	// Intercept SIGHUP and SIGINT
+	if (sigaction(SIGHUP, &sa, NULL) == -1) {
+		perror("Error: cannot handle SIGHUP"); // Should not happen
+	}
+
 	while (1) {
-		c = getopt(argc, argv, "u:o:");
+		c = getopt(argc, argv, "u:o:s:");
 		/* Detect the end of the options. */
 		if (c == -1)
 			break;
@@ -47,6 +71,9 @@ main(int argc, char *argv[])
 			break;
 		case 'o':
 			fout = optarg;
+			break;
+		case 's':
+			speedtest = 1;
 			break;
 		}
 	}
@@ -63,13 +90,30 @@ fetch_files(char *urls, char *fout)
 {
 	FILE           *fetch_out, *file_out;
 	struct url_stat	ustat;
-	off_t		total_bytes, current_bytes, fsize;
+	off_t		total_bytes, fsize;
 	uint8_t		block  [4096];
 	size_t		chunk;
 	int		progress  , last_progress;
 	int		nsuccess = 0;	/* Number of files successfully
 					 * downloaded */
 	int		lprg = 0;
+	struct sigaction sa;
+	sigset_t mask;
+
+	sa.sa_handler = &handle_sigalrm; // Intercept and ignore SIGALRM
+	sa.sa_flags = SA_RESETHAND; // Remove the handler after first signal
+	sigfillset(&sa.sa_mask);
+	sigaction(SIGALRM, &sa, NULL);
+
+	// Get the current signal mask
+	sigprocmask(0, NULL, &mask);
+
+	// Unblock SIGALRM
+	sigdelset(&mask, SIGALRM);
+
+//	// Wait with this mask
+//	alarm(1);
+//	sigsuspend(&mask);
 
 	progress = 0;
 	total_bytes = 0;
@@ -81,9 +125,12 @@ fetch_files(char *urls, char *fout)
 	fetch_out = fetchXGetURL(urls, &ustat, "");
 	if (fetch_out == NULL)
 		return 1;
+
 	file_out = fopen(fout, "w+");
 
-	printf("Size: %d Mb\n", ((int)total_bytes / 1024 / 1024));
+	if (speedtest!=1)
+		printf("Size: %d Mb\n", ((int)total_bytes / 1024 / 1024));
+
 	current_bytes = 0;
 	while ((chunk = fread(block, 1, sizeof(block), fetch_out)) > 0) {
 		if (fwrite(block, 1, chunk, file_out) < chunk)
@@ -98,14 +145,15 @@ fetch_files(char *urls, char *fout)
 		}
 		if ((progress % 10 == 0) && (lprg != progress)) {
 			lprg = progress;
-			printf("Progress: %d%% \n", progress);
+			if (speedtest!=1)
+				printf("Progress: %d%% \n", progress);
 		}
 	}
 
 	if (ustat.size > 0 && fsize < ustat.size) {
 		if (fetchLastErrCode == 0) {
 			//small chunk
-				fclose(fetch_out);
+			fclose(fetch_out);
 			fclose(file_out);
 			return 0;
 		}
@@ -115,4 +163,43 @@ fetch_files(char *urls, char *fout)
 	fclose(fetch_out);
 	fclose(file_out);
 	return (0);
+}
+
+
+void handle_signal(int signal) {
+	const char *signal_name;
+	sigset_t pending;
+
+	// Find out which signal we're handling
+	switch (signal) {
+		case SIGHUP:
+			signal_name = "SIGHUP";
+			break;
+		case SIGUSR1:
+			signal_name = "SIGUSR1";
+			break;
+		case SIGINT:
+			printf("Caught SIGINT, exiting now\n");
+			exit(0);
+		default:
+			fprintf(stderr, "Caught wrong signal: %d\n", signal);
+			return;
+	}
+
+	/*
+	 * Please note that printf et al. are NOT safe to use in signal handlers.
+	 * Look for async safe functions.
+	 */
+	printf("%ld\n",current_bytes);
+
+	exit(0);
+}
+
+
+void handle_sigalrm(int signal) {
+	if (signal != SIGALRM) {
+		fprintf(stderr, "Caught wrong signal: %d\n", signal);
+	}
+
+	printf("Got sigalrm, do_sleep() will end\n");
 }
