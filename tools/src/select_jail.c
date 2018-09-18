455e73a08f14540ff1e4e428d212982353a0ad8e
olevole <olevole@olevole.ru>
2018-09-19 02:42:18 +0300

    select_jail: add description via optional ascii file

--- tools/src/select_jail.c
+++ tools/src/select_jail.c
@@ -1,4 +1,4 @@
-// CBSD Project 2017
+// CBSD Project 2017-2018
 // Oleg Ginzburg <olevole@olevole.ru>
 // 0.1
 // Todo: fast-written and confusing code with magic numbers, need to refactoring
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <sys/ioctl.h>
+#include <dirent.h>
 
 #define KEY_UP		65
 #define KEY_DOWN	66
@@ -27,9 +28,14 @@
 #define LGREEN	"\033[1;32m"
 #define CYAN	"\033[0;36m"
 #define SELECT  "\033[41m"
+#define WHITE	"\033[1;37m"
+#define LYELLOW "\033[1;33m"
 
-#define BUFLEN	1024
+#define BUFLEN	2048
 #define MAXJNAME 128
+#define MAXFNAME 1024
+#define MAXFULLPATH 1024
+#define MAXDESCRLEN 2048
 
 #define CREATE(result, type, number)  do {\
 	if (!((result) = (type *) calloc ((number), sizeof(type))))\
@@ -38,9 +44,13 @@
 struct winsize w;
 
 struct item_data {
-	int id;
-	int cid;
-	char name[MAXJNAME];
+	int id;                // sequence
+	int cid;               // letter by index for hot key
+	char name[MAXFNAME];	//name of file
+	//char name[MAXJNAME];
+	char ext[MAXFNAME];		//extension (less size?)
+	char fullpath[MAXFULLPATH];	//realpath к файлу
+	char descr[MAXDESCRLEN];	//descr
 	char node[MAXJNAME];
 	int active;
 	struct item_data *next;
@@ -64,9 +74,9 @@ int mygetch(void)
 	return c;
 }
 
-int usage()
+int usage(char *progname)
 {
-	printf("Usage: select_jail <filename>\n");
+	printf("Usage: %s <directory> <file-for-output>\n",progname);
 	exit(1);
 }
 
@@ -96,6 +106,17 @@ int print()
 	return 0;
 }
 
+/* qsort C-string comparison function */ 
+static int compare_fun (const void *p, const void *q)
+{
+    const char *l= p;
+    const char *r= q;
+    int cmp;
+
+    cmp= strcmp (l, r);
+    return cmp;
+}
+
 void reverse() {
 	// curdata traverses the list, first is reset to empty list.
 	struct item_data *curdata = item_list, *nxtNode;
@@ -123,21 +144,39 @@ int main(int argc, char **argv)
 	struct item_data *m_item;
 	char *token, *string, *tofree;
 	FILE *fp;
+    FILE *fo;
 	char buf[BUFLEN];
+    char buf2[BUFLEN];
 	int x=0;
 	int pass=0;		// symbol range treshold
 	int id;
 	int special;
 	int manual_input=0;	// if windows size less then number of elements
 	int second_id=1;
+	DIR *dirp;
+	struct dirent *dp;
+	char ext[128];		//extenstion for scan
+	char descrfile[MAXFULLPATH];
+    char fullpath[MAXFULLPATH];
+	char descr[MAXDESCRLEN];
+    int listmax=0; 
+    int n=0; 
+    char mylist[100][MAXFNAME];
+    
+	if (argc<3) usage(argv[0]);
+    memset(ext,0,sizeof(ext));
 
-	if (argc<2) usage();
+    if (argv[3]!=NULL) {
+		strcpy(ext,argv[3]);
+	} else {
+		strcpy(ext,"img");
+	}
 
-	fp=fopen(argv[1],"r");
+	dirp = opendir(argv[1]);
 
-	if (!fp) {
-		fprintf(stderr,"Unable to open file %s\n",argv[1]);
-		exit(2);
+	if (dirp == NULL) {
+		fprintf(stderr,"Unable to opendir: %s\n",argv[1]);
+		exit(1);
 	}
 
 	// get terminal size
@@ -148,62 +187,136 @@ int main(int argc, char **argv)
 	tmp_id=96;
 	id=0;
 
-	// load data
-	while (!feof(fp)) {
+   	descr[0]='\0';
 
+	// load data
+    while ((dp = readdir(dirp)) != NULL && listmax < sizeof mylist / sizeof mylist[0]) {
+        if (dp->d_name[0]=='.') continue;
+        strncpy(mylist[listmax++] , dp->d_name, MAXFNAME);
+    }
+    (void)closedir(dirp);
+	free(dp);
+    if (listmax<1) exit(0);
+
+    qsort( mylist , listmax , sizeof(mylist[0]), compare_fun);
+
+    for(n=0;n<listmax;n++)
+    {
 		memset(buf,0,sizeof(buf));
-		fscanf(fp,"%s",buf);
-		if (feof(fp)) break;
+		strcpy(buf,mylist[n]);
 		CREATE(m_item, struct item_data, 1);
 		tofree = string = strdup(buf);
 		assert(string != NULL);
 
 		x=0;
-		while ((token = strsep(&string, ":")) != NULL) {
+		while ((token = strsep(&string, ".")) != NULL) {
 			switch (x) {
-				case 0: m_item->active=atoi(token); break;
-				case 1: strcpy(m_item->name,token); break;
-				case 2: strcpy(m_item->node,token); break;
+				case 0: strcpy(m_item->name,token); break;
+				case 1: strcpy(m_item->ext,token); break;
 			}
 			x++;
 		}
 
 		free(tofree);
 
-		if (x!=3) {
-			fprintf(stdout,"Warning: not <active>.int:<name>.str:<node>.str format: [%s], skipp\n",argv[1]);
-		} else {
+		if (!strncmp(m_item->ext,ext,strlen(ext)))  {
+			// пропустим latest симлинк
+			if(!strncmp(m_item->name,"latest",6))
+				continue;
 
 			tmp_id++;
 			id++;
 
-			//manage index here
-			//first stage: a-z -> A
+			// первый проход по буквенному индексу
+			// стадия: a-z -> A
 			if ((tmp_id>122)&&(pass==0)) {
 				tmp_id=65;
 				pass=1;
 			}
 
-			//second stage: A-Z -> 1
+			// вторая стадия: A-Z -> 1
 			if ((tmp_id>90)&&(pass==1)) {
 				tmp_id=49;
 				pass=2;
 			}
 
-			// too many jails, begin from 'a' again
-			// 1-9 -> a, reset stages to zero
+			// слишком много файлов, начинаем с 'a' опять
+			// сбрасываем стэдж на начало
 			if ((tmp_id>57)&&(pass==2)) {
 				tmp_id=97;
 				pass=3;
 			}
 
-			// loaded
+			// создаем запись
 			m_item->cid = tmp_id;
 			m_item->id = id;
 			m_item->next = item_list;
+			memset(m_item->fullpath,0,sizeof(m_item->fullpath));
+			sprintf(m_item->fullpath,"%s/%s",argv[1],buf);
+
+            m_item->id=1;
+
+			fprintf(stderr,"Pattern file found: %s\n",buf);
+            memset(fullpath,0,sizeof(fullpath));
+            sprintf(fullpath,"%s/%s",argv[1],buf);
+            fo=fopen(fullpath,"r");
+
+            if (!fo) {
+				fprintf(stderr,"Unable to open file %s\n",fullpath);
+				break;
+		    }
+
+            memset(buf2,0,sizeof(buf2));
+            fscanf(fo,"%s",buf2);
+            //if (feof(fo)) break;
+            tofree = string = strdup(buf2);
+            assert(string != NULL);
+                                        
+            x=0;
+            while ((token = strsep(&string, ":")) != NULL) {
+                switch (x) {
+                        case 0: m_item->active=atoi(token); break;
+                        case 1: strcpy(m_item->name,token); break;
+                        case 2: strcpy(m_item->node,token); break;
+                }
+                x++;
+            }
+
+            fclose(fo);
+
+            free(tofree);
+
+            if (x!=3) {
+                fprintf(stdout,"Warning: not <active>.int:<name>.str:<node>.str format: [%s], skipp\n",fullpath);
+            }
+
+			memset(descrfile,0,MAXFULLPATH);
+			sprintf(descrfile,"%s/%s.descr",argv[1],m_item->name);
+			fp=fopen(descrfile,"r");
+			if (fp) {
+				fprintf(stderr,"Found descr!\n");
+				memset(m_item->descr,0,MAXDESCRLEN);
+				fgets(m_item->descr, MAXDESCRLEN, fp);
+//				strcpy(m_item->descr,"OLALALLALALA BLABLA");
+				fclose(fp);
+				for (i=0;i<strlen(m_item->descr); i++)
+					if(m_item->descr[i]=='\n') m_item->descr[i]='\0';
+				fprintf(stderr,"new descr: %s descr!\n",m_item->descr);
+			} else {
+				(m_item->descr[0]='\0');
+			}
+
 			item_list = m_item;
 			max_choice++;
+		} else {
+			fprintf(stderr,"[debug] warning: no %s extension (%s), skipp: [%s]\n",ext,m_item->ext,buf);
+			continue;
 		}
+	}
+
+	if (max_choice==1) {
+		fprintf(stderr,"Files not found: %s\n",argv[1]);
+		exit(1);
 	}
 
 	// reverse order
@@ -231,10 +344,14 @@ int main(int argc, char **argv)
 		{
 			item++;
 			if (m_item->active==0) {
-
-				if (item==cur_choice)
+				if (item==cur_choice) {
 					printf("%s",SELECT);
-
+					if(strlen(m_item->descr)) {
+						sprintf(descr,"%s%s%s%s",BOLD,LYELLOW,m_item->descr,NORMAL);
+						} else {
+							memset(descr,0,MAXDESCRLEN);
+						}
+					}
 				if (item==cur_choice) {
 					if (manual_input==1)
 						printf(" %s%d .. %s%s on %s%s\n",BOLD,m_item->id,GREEN,m_item->name,m_item->node,NORMAL);
@@ -249,6 +366,11 @@ int main(int argc, char **argv)
 			} else {
 				if (item==cur_choice) {
 					printf("%s",SELECT);
+                    if (strlen(m_item->descr)) {
+						sprintf(descr,"%s%s%s%s",BOLD,LYELLOW,m_item->descr,NORMAL);
+					} else {
+							memset(descr,0,MAXDESCRLEN);
+					}
 					if (manual_input==1)
 						printf(" %s%d .. %s%s on %s%s\n",BOLD,m_item->id,LGREEN,m_item->name,m_item->node,NORMAL);
 					else
@@ -299,6 +421,10 @@ int main(int argc, char **argv)
 				exit(2);
 			}
 		}
+
+        // show descr if not cancel
+		if (cur_choice>1)
+			printf("%s",descr);
 
 		i = mygetch();
 		if ( i == 27 ) { // if the first value is esc, [
@@ -371,7 +497,7 @@ int main(int argc, char **argv)
 		{
 			if (cur_choice == tmp_id) {
 				fprintf(stderr,"%s\n",m_item->name);
-				fp=fopen(argv[1],"w");
+				fp=fopen(argv[2],"w");
 				fputs(m_item->name,fp);
 				fclose(fp);
 				exit(0);
