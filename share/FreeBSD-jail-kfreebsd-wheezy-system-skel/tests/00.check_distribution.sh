ca3e91ff375237ee25fa61087676f5d0a251efce
olevole <olevole@olevole.ru>
2018-09-28 02:11:02 +0300

    add debug_engine for launch bhyve via gdb/lldb
    make xhci configurable

--- share/FreeBSD-jail-kfreebsd-wheezy-system-skel/tests/00.check_distribution.sh
+++ share/FreeBSD-jail-kfreebsd-wheezy-system-skel/tests/00.check_distribution.sh
@@ -20,9 +20,12 @@ workdir="${cbsd_workdir}"
 . ${tools}
 
 SRC_MIRROR="http://ftp.us.debian.org/debian"
+# + http://deb.debian.org/debian/
 customskel="${sharedir}/FreeBSD-jail-kfreebsd-wheezy-skel"
 
 [ -z "${jname}" ] && err 1 "${MAGENTA}Empty jname${NORMAL}"
+
+[ ! -d ${customskel} ] && /bin/mkdir -p ${customskel}
 
 if [ ! -x /usr/local/sbin/debootstrap ]; then
 	err 1 "${MAGENTA}No such debootstrap. Please ${GREEN}pkg install debootstrap${MAGENTA} it.${NORMAL}"
@@ -36,7 +39,9 @@ if [ ! -f ${customskel}/bin/bash ]; then
 	export INTER=1
 
 	if getyesno "Shall i download distribution via deboostrap from ${SRC_MIRROR}?"; then
-		debootstrap wheezy ${customskel} ${SRC_MIRROR}
+		${ECHO} "${MAGENTA}debootstrap ${LCYAN}--include=openssh-server,locales,joe,rsync,sharutils,psmisc,htop,patch,less,apt --components main,contrib ${LYELLOW}wheezy ${MAGENTA}${customskel} ${SRC_MIRROR}${NORMAL}"
+		debootstrap --include=openssh-server,locales,joe,rsync,sharutils,psmisc,htop,patch,less,apt --components main,contrib wheezy ${customskel} ${SRC_MIRROR}
+		chroot ${customskel} dpkg -i /var/cache/apt/archives/*.deb
 	else
 		echo "No such distribution"
 		exit 1
