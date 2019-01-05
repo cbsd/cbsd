b9b6a27becf1c3f15f6559aa4cff409dae2458db
olevole <olevole@olevole.ru>
2019-01-05 20:00:34 +0300

    bhyve: add exit behavior settings
    current opportunities:
      destroy - The domain will be terminated completely and all resources released
      restart - The domain will be terminated and then restarted with the same configuration

--- share/bhyverun.sh
+++ share/bhyverun.sh
@@ -1,9 +1,115 @@
 #!/bin/sh
 
-while getopts "c:d:g:l:r:" opt; do
+on_crash_status()
+{
+	local _ret=0
+
+	case "${on_crash}" in
+		restart)
+			_ret=1
+			;;
+		destroy)
+			_ret=0
+			;;
+		*)
+			logger -t bhyverun.sh "unknown value for on_poweroff: ${on_poweroff}"
+			_ret=0	# use default behavior
+			;;
+	esac
+
+	return ${_ret}
+}
+
+# FreeBSD 11
+# EXIT STATUS
+#     Exit status indicates how the VM was terminated:
+#     0       rebooted
+#     1       powered off
+#     2       halted
+#     3       triple fault
+# FreeBSD 12+
+#     4       exited due to an error
+# route for exit codes and  exit_action:
+#   on_poweroff='destroy'
+#   on_reboot='restart'
+#   on_crash='destroy'
+# return 0 when we should stop bhyve loop
+# return 1 when show must go on
+exit_action_mode()
+{
+	local _ret
+
+	if [ ${exit_action} = "0" -a ${bhyve_exit} -eq 0 ]; then
+		# no exit_action mode but normal reboot
+		return 1
+	fi
+
+	[ "${exit_action}" != "1" ] && return 0
+
+	# use on_poweroff/on_reboot/on_crash settings
+	logger -t bhyverun.sh "in exit_action mode"
+	_ret=0
+
+	case ${bhyve_exit} in
+		0)
+			case "${on_reboot}" in
+				restart)
+					_ret=1
+					;;
+				destroy)
+					_ret=0
+					;;
+				*)
+					logger -t bhyverun.sh "unknown value for on_reboot: ${on_reboot}"
+					_ret=1	# use default behavior
+					;;
+			esac
+			logger -t bhyverun.sh "bhyve ${jname} was rebooted, exit_action_mode ret: ${_ret}"
+			;;
+		1)
+			case "${on_poweroff}" in
+				restart)
+					_ret=1
+					;;
+				destroy)
+					_ret=0
+					;;
+				*)
+					logger -t bhyverun.sh "unknown value for on_poweroff: ${on_poweroff}"
+					_ret=0	# use default behavior
+					;;
+			esac
+			logger -t bhyverun.sh "bhyve ${jname} was poweroff, exit_action_mode ret: ${_ret}"
+			;;
+		2)
+			on_crash_status
+			_ret=$?
+			logger -t bhyverun.sh "bhyve ${jname} was halted, exit_action_mode ret: ${_ret}"
+			;;
+		3)
+			on_crash_status
+			_ret=$?
+			logger -t bhyverun.sh "bhyve ${jname} was tripple fault, exit_action_mode ret: ${_ret}"
+			;;
+		4)
+			on_crash_status
+			_ret=$?
+			logger -t bhyverun.sh "bhyve ${jname} exited due to an error, exit_action_mode ret: ${_ret}"
+			;;
+		*)
+			on_crash_status
+			_ret=$?
+			logger -t bhyverun.sh "bhyve ${jname} exited with unknown error ${bhyve_exit}, exit_action_mode ret: ${_ret}"
+			;;
+	esac
+	return ${_ret}
+}
+
+while getopts "c:d:e:g:l:r:" opt; do
 	case "${opt}" in
 		c) conf="${OPTARG}" ;;
 		d) debug="${OPTARG}" ;;
+		e) exit_action="${OPTARG}" ;;
 		g) debug_engine="${OPTARG}" ;;
 		l) orig_logfile="${OPTARG}" ;;
 		r) restore_checkpoint="${OPTARG}" ;;
@@ -13,6 +119,7 @@ done
 
 [ -z "${debug_engine}" ] && debug_engine="none"
 [ -z "${xhci}" ] && xhci=0
+[ -z "${exit_action}" ] && exit_action=0	# use on_poweroff/on_reboot/on_crash settings: disabled by default
 [ ! -f "${conf}" ] && exit 0
 . ${conf}
 
@@ -46,18 +153,22 @@ fi
 . ${workdir}/cbsd.conf
 
 freebsdhostversion=$( ${miscdir}/elf_tables --ver /bin/sh )
+orig_vnc_args="${vnc_args}"
+
 while [ ! -f /tmp/bhyvestop.${jname}.lock  ]; do
+
+	vnc_args="${orig_vnc_args}"
 
 	/usr/sbin/bhyvectl --vm=${jname} --destroy > /dev/null 2>&1
 	/usr/bin/truncate -s0 ${vm_logfile}
 
 	if [ ${cd_boot_once} -eq 0 ]; then
 		echo "DEBUG: ${bhyveload_cmd}" | /usr/bin/tee -a ${vm_logfile}
-		eval "${bhyveload_cmd}"
+		eval "${bhyveload_cmd}" | /usr/bin/tee -a ${vm_logfile}
 	else
 		echo "Boot from CD" | /usr/bin/tee -a ${vm_logfile}
 		echo "DEBUG: ${bhyveload_cmd}" | /usr/bin/tee -a ${vm_logfile}
-		eval "${bhyveload_cmd}"
+		eval "${bhyveload_cmd}" | /usr/bin/tee -a ${vm_logfile}
 	fi
 
 	case "${vm_boot}" in
@@ -65,12 +176,12 @@ while [ ! -f /tmp/bhyvestop.${jname}.lock  ]; do
 			# add ,wait args when boot from CD
 			if [ "${vm_efi}" != "none" ]; then
 				if [ -n "${vnc_args}" -a "${vm_vnc_port}" != "1" ]; then
-					orig_vnc_args="${vnc_args}"
+					#orig_vnc_args="${vnc_args}"
 					# bhyve_vnc_vgaconf before wait
 					if [ "${bhyve_vnc_vgaconf}" != "io" ]; then
 						[ -n "${bhyve_vnc_vgaconf}" ] && vnc_args="${vnc_args},vga=${bhyve_vnc_vgaconf}"
 					fi
-					orig_vnc_args="${vnc_args}"
+					#orig_vnc_args="${vnc_args}"
 					if [ "${cd_vnc_wait}" = "1" ]; then
 						echo "Waiting for first connection via VNC to starting VMs..."
 						vnc_args="${vnc_args},wait"
@@ -150,9 +261,11 @@ while [ ! -f /tmp/bhyvestop.${jname}.lock  ]; do
 	debug_bhyve_cmd="/usr/sbin/bhyve ${bhyve_flags} -c ${vm_cpus} -m ${vm_ram} ${add_bhyve_opts} ${hostbridge_args} ${virtio_9p_args} ${uefi_boot_args} ${dsk_args} ${dsk_controller_args} ${cd_args} ${nic_args} ${nvme_args} ${virtiornd_args} ${pci_passthru_args} ${vnc_args} ${xhci_args} ${lpc_args} ${console_args} ${efi_args} ${checkpoint_args} ${live_migration_args} ${jname}"
 
 	echo "[debug] ${bhyve_cmd}"
-	logger -t CBSD "[debug] ${bhyve_cmd}"
+	logger -t bhyverun.sh "[debug] ${bhyve_cmd}"
 	echo "cmd: ${bhyve_cmd}" >> ${vm_logfile}
 	echo "-----" >>  ${vm_logfile}
+
+	bhyve_exit=0
 
 	case "${debug_engine}" in
 		gdb)
@@ -171,7 +284,7 @@ while [ ! -f /tmp/bhyvestop.${jname}.lock  ]; do
 			echo
 			echo "/usr/bin/lockf -s -t0 /tmp/bhyveload.${jname}.lock ${gdb_cmd} -batch -x /tmp/cmds.$$ --args ${debug_bhyve_cmd}"
 			/usr/bin/lockf -s -t0 /tmp/bhyveload.${jname}.lock ${gdb_cmd} -ex run --args ${debug_bhyve_cmd}
-			ret=$?
+			bhyve_exit=$?
 			/bin/rm -f /tmp/cmds.$$
 			;;
 		lldb)
@@ -183,24 +296,40 @@ while [ ! -f /tmp/bhyvestop.${jname}.lock  ]; do
 			echo
 			echo "/usr/bin/lockf -s -t0 /tmp/bhyveload.${jname}.lock /usr/bin/lldb -- ${debug_bhyve_cmd}"
 			/usr/bin/lockf -s -t0 /tmp/bhyveload.${jname}.lock /usr/bin/lldb -- ${debug_bhyve_cmd}
-			ret=$?
+			bhyve_exit=$?
 			/bin/rm -f /tmp/cmds.$$
 			;;
 		*)
 			/usr/bin/lockf -s -t0 /tmp/bhyveload.${jname}.lock ${bhyve_cmd} >> ${vm_logfile} 2>&1
-			ret=$?
+			bhyve_exit=$?
 			;;
 	esac
 
-	if [ ${ret} -ne 0 ]; then
+	ret=0
+	exit_action_mode
+	ret=$?
+
+	if [ ${ret} -eq 0 ]; then
+		# exit from loop
 		touch /tmp/bhyvestop.${jname}.lock
-		echo "Exit code: ${ret}. See ${vm_logfile} for details"
-		echo
-		tail -n50 ${vm_logfile}
-		echo "Sleep 15 seconds..."
-		sleep 15
+		echo "bhyve exit code: ${bhyve_exit}. exit_action settings: ${exit_action}, exit_action_mode ret: ${ret}: must stoppped"
+		logger -t bhyverun.sh "bhyve exit code: ${bhyve_exit}. exit_action settings: ${exit_action}, exit_action_mode ret: ${ret}: must stopped"
+		case ${bhyve_exit} in
+			0|1)
+				# normal exit
+				/bin/rm -f ${vm_logfile}
+				;;
+			*)
+				# bhyve error or crash
+				echo "See ${vm_logfile} for details"
+				echo
+				/usr/bin/tail -n50 ${vm_logfile}
+				echo "Sleep 1 seconds..."
+				sleep 1
+		esac
 	else
-		/bin/rm -f ${vm_logfile}
+		echo "bhyve exit code: ${bhyve_exit}. exit_action settings: ${exit_action}, exit_action_mode ret: ${ret}: must continue"
+		logger -t bhyverun.sh "bhyve exit code: ${bhyve_exit}. exit_action settings: ${exit_action}, exit_action_mode ret: ${ret}: must continue"
 	fi
 
 	# restore original value
@@ -237,7 +366,7 @@ done
 # live migration todo
 # check for bhyve migrated successfull to me ( bregister and/or bstatus passed ? )
 
-# CBSD QUEUE
+# bhyverun.sh QUEUE
 [ -z "${cbsd_queue_name}" ] && cbsd_queue_name="/clonos/bhyvevms/"
 
 if [ -x "${moduledir}/cbsd_queue.d/cbsd_queue" ]; then
@@ -249,13 +378,7 @@ fi
 # extra destroy
 /usr/bin/nice -n ${nice} /usr/sbin/bhyvectl --vm=${jname} --destroy > /dev/null 2>&1 || true
 /bin/rm -f /tmp/bhyvestop.${jname}.lock
-# extra stop
+# extra stop/cleanup
 /usr/local/bin/cbsd bstop cbsd_queue_name=none jname=${jname}
-
-if [ ${jailed} -eq 0 ]; then
-	for i in ${mytap}; do
-		/sbin/ifconfig ${i} destroy
-	done
-fi
 
 exit ${bhyve_exit}
