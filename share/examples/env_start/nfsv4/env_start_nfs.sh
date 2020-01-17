794b7f0fb203acf9392e92c2f9ae145e23c2c9c7
olevole <olevole@olevole.ru>
2020-01-17 20:00:47 +0300

    wip: mnt_start hook for bhyve

--- share/examples/env_start/nfsv4/env_start_nfs.sh
+++ share/examples/env_start/nfsv4/env_start_nfs.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # CBSD Project, custom script for mnt_start hook sample ( NFSv4 client )
+# for jail and bhyve environments
 
 # read config from /root/etc/env_start_nfs.conf if file exist
-# see /usr/local/cbsd/share/examples/env_start/jail/env_start_nfs.conf as sample
+# see /usr/local/cbsd/share/examples/env_start/nfsv4/env_start_nfs.conf as sample
 [ -r /root/etc/env_start_nfs.conf ] && . /root/etc/env_start_nfs.conf
 
 # NFS server 127.0.0.1 as default: change this ( e.g via /root/etc/env_start.nfs.conf
-# see /usr/local/cbsd/share/examples/env_start/jail/env_start_nfs.conf as sample
+# see /usr/local/cbsd/share/examples/env_start/nfsv4/env_start_nfs.conf as sample
 : ${NFS_SERVER:=127.0.0.1}
 : ${NFS_SERVER_ROOT_DIR:=/nfs}
 : ${MOUNT_RCCONF:=0}
+: ${MOUNT_FSTAB:=0}
 
+[ -z "${MOUNT_NFS_OPT}" ] && MOUNT_NFS_OPT="-orw -overs=4"
 [ -z "${NFS_SERVER_DATA_DIR}" ] && NFS_SERVER_DATA_DIR="${NFS_SERVER_ROOT_DIR}/data"
 [ -z "${NFS_SERVER_RCCONF_DIR}" ] && NFS_SERVER_RCCONF_DIR="${NFS_SERVER_ROOT_DIR}/rcconf"
 [ -z "${NFS_SERVER_FSTAB_DIR}" ] && NFS_SERVER_FSTAB_DIR="${NFS_SERVER_ROOT_DIR}/fstab"
@@ -57,11 +60,11 @@ is_mounted()
 # MAIN()
 while getopts "d:f:j:r:s:" opt; do
 	case "${opt}" in
-		d) data="${OPTARG}" ;;
-		f) fstab="${OPTARG}" ;;
-		j) jname="${OPTARG}" ;;
-		r) rcconf="${OPTARG}" ;;
-		s) sysdata="${OPTARG}" ;;
+		d) data="${OPTARG}" ;;		# env data directory (required)
+		f) fstab="${OPTARG}" ;;		# env fstab directory (optional)
+		j) jname="${OPTARG}" ;;		# env name (required)
+		r) rcconf="${OPTARG}" ;;	# env rcconf directory (optional)
+		s) sysdata="${OPTARG}" ;;	# env system directory (required)
 	esac
 	shift $(( ${OPTIND} - 1 ))
 done
@@ -76,9 +79,12 @@ if [ -z "${sysdata}" ]; then
 	echo "Empty sysdata, use -s <sysdata>"
 fi
 
-echo "Mount data for jname: ${jname}"
-echo "data: ${data}"
-echo "sysdata: ${sysdata}"
+[ -z "${fstab}" ] && MOUNT_FSTAB=0		# no fstab args
+[ -z "${rcconf}" ] && MOUNT_RCCONF=0		# no rcconf args
+
+echo "Mount data for jname: ${jname}" 1>&2
+echo "data: ${data}" 1>&2
+echo "sysdata: ${sysdata}" 1>&2
 
 trap "print_info" HUP INT ABRT BUS TERM EXIT
 ret=0
@@ -87,7 +93,7 @@ if is_mounted ${data}; then
 	echo "[debug] ${data} already mounted!" 1>&2
 else
 	remote_dir="${NFS_SERVER_DATA_DIR}/${jname}"
-	cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs -orw -overs=4 ${NFS_SERVER}:${remote_dir} ${data}"
+	cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs ${MOUNT_NFS_OPT} ${NFS_SERVER}:${remote_dir} ${data}"
 	${cmd}
 	ret=$?
 fi
@@ -98,7 +104,7 @@ if is_mounted ${sysdata}; then
 	echo "[debug] ${sysdata} already mounted!" 1>&2
 else
 	remote_dir="${NFS_SERVER_SYSTEM_DIR}/${jname}"
-	cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs -orw -overs=4 ${NFS_SERVER}:${remote_dir} ${sysdata}"
+	cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs ${MOUNT_NFS_OPT} ${NFS_SERVER}:${remote_dir} ${sysdata}"
 	${cmd}
 	ret=$?
 fi
@@ -111,20 +117,23 @@ if [ ${MOUNT_RCCONF} -eq 1 ]; then
 		echo "[debug] ${rcconf} already mounted!" 1>&2
 	else
 		remote_dir="${NFS_SERVER_RCCONF_DIR}/${jname}"
-		cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs -orw -overs=4 ${NFS_SERVER}:${remote_dir} ${rcconf}"
+		cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs ${MOUNT_NFS_OPT} ${NFS_SERVER}:${remote_dir} ${rcconf}"
 		${cmd}
 		ret=$?
 	fi
 	[ ${ret} -ne 0 ] && exit ${ret}
 fi
 
-if is_mounted ${fstab}; then
-	echo "[debug] ${fstab} already mounted!" 1>&2
-else
-	remote_dir="${NFS_SERVER_FSTAB_DIR}/${jname}"
-	cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs -orw -overs=4 ${NFS_SERVER}:${remote_dir} ${fstab}"
-	${cmd}
-	ret=$?
+# mount fstab if necessary, see config file
+if [ ${MOUNT_FSTAB} -eq 1 ]; then
+	if is_mounted ${fstab}; then
+		echo "[debug] ${fstab} already mounted!" 1>&2
+	else
+		remote_dir="${NFS_SERVER_FSTAB_DIR}/${jname}"
+		cmd="timeout ${NFS_MOUNT_TIMEOUT} mount_nfs ${MOUNT_NFS_OPT} ${NFS_SERVER}:${remote_dir} ${fstab}"
+		${cmd}
+		ret=$?
+	fi
 fi
 
 # clean/unset trap
