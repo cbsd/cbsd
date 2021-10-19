2974817bebc25db77b74f9bfd26aadd6f8a86753
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2021-10-19 18:20:16 +0300

    Update CBSDCompletion.bash

--- share/autocompletion/bash/CBSDCompletion.bash
+++ share/autocompletion/bash/CBSDCompletion.bash
@@ -8,22 +8,22 @@ _cbsd () {
     # We only get CBSD subcomand list if there is not already one defined
     if [ ${#COMP_WORDS[@]} -eq 2 ]; then
         # Show only the options that start with $cur
-        COMPREPLY=( $( compgen -W "$( cbsd help nodesc|sed -r '/^\s*$/d'|grep -v ' ' )" -- $cur ) )
+        COMPREPLY=( $( compgen -W "$( cbsd help nodesc|sed -r '/^\s*$/d'|grep -v ' ' )" -- "$cur" ) )
         return 0
     # If we have the subcommand, we check for arguments only for some common used commands
     elif [ ${#COMP_WORDS[@]} -eq 3 ]; then
         case ${COMP_WORDS[1]} in
             bconfig)
                 VMS=$(env NOCOLOR=1 cbsd bls display=jname header=0|awk '{print"jname="$1}'|tr -s '\n' ' ')
-                COMPREPLY=( $( compgen -W "$VMS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$VMS" -- "$cur" ) )
                 ;;
             blogin)
                 VMS=""
                 N=$(env NOCOLOR=1 cbsd bls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n "$I"|head -n 1)
                     if [ "$STATE" == "On" ]; then
-                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n $I|head -n 1)
+                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n "$I"|head -n 1)
                         if [ "$VMS" == "" ]; then
                             VMS=$VM
                         else
@@ -31,15 +31,15 @@ _cbsd () {
                         fi
                     fi
                 done
-                COMPREPLY=( $( compgen -W "$VMS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$VMS" -- "$cur" ) )
                 ;;
             bstart)
                 VMS=""
                 N=$(env NOCOLOR=1 cbsd bls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n "$I"|head -n 1)
                     if [ "$STATE" == "Off" ]; then
-                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n $I|head -n 1)
+                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n "$I"|head -n 1)
                         if [ "$VMS" == "" ]; then
                             VMS=$VM
                         else
@@ -47,15 +47,15 @@ _cbsd () {
                         fi
                     fi
                 done
-                COMPREPLY=( $( compgen -W "$VMS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$VMS" -- "$cur" ) )
                 ;;
             bstop)
                 VMS=""
                 N=$(env NOCOLOR=1 cbsd bls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                    STATE=$(env NOCOLOR=1 cbsd bls display=status header=0|tail -n "$I"|head -n 1)
                     if [ "$STATE" == "On" ]; then
-                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n $I|head -n 1)
+                        VM=$(env NOCOLOR=1 cbsd bls display=jname header=0|tail -n "$I"|head -n 1)
                         if [ "$VMS" == "" ]; then
                             VMS=$VM
                         else
@@ -63,23 +63,23 @@ _cbsd () {
                         fi
                     fi
                 done
-                COMPREPLY=( $( compgen -W "$VMS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$VMS" -- "$cur" ) )
                 ;;
             bremove)
                 VMS=$(env NOCOLOR=1 cbsd bls display=jname header=0|tr -s '\n' ' ')
-                COMPREPLY=( $( compgen -W "$VMS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$VMS" -- "$cur" ) )
                 ;;
             jconfig)
                 JAILS=$(env NOCOLOR=1 cbsd jls display=jname header=0|awk '{print"jname="$1}'|tr -s '\n' ' ')
-                COMPREPLY=( $( compgen -W "$JAILS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$JAILS" -- "$cur" ) )
                 ;;
             jlogin)
                 JAILS=""
                 N=$(env NOCOLOR=1 cbsd jls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n "$I"|head -n 1)
                         if [ "$STATE" == "On" ]; then
-                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n $I|head -n 1)
+                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n "$I"|head -n 1)
                             if [ "$JAILS" == "" ]; then
                                 JAILS=$JAIL
                             else
@@ -87,15 +87,15 @@ _cbsd () {
                             fi
                         fi
                 done
-                COMPREPLY=( $( compgen -W "$JAILS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$JAILS" -- "$cur" ) )
                 ;;
             jstart)
                 JAILS=""
                 N=$(env NOCOLOR=1 cbsd jls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n "$I"|head -n 1)
                         if [ "$STATE" == "Off" ]; then
-                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n $I|head -n 1)
+                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n "$I"|head -n 1)
                             if [ "$JAILS" == "" ]; then
                                 JAILS=$JAIL
                             else
@@ -103,15 +103,15 @@ _cbsd () {
                             fi
                         fi
                 done
-                COMPREPLY=( $( compgen -W "$JAILS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$JAILS" -- "$cur" ) )
                 ;;
             jstop)
                 JAILS=""
                 N=$(env NOCOLOR=1 cbsd jls display=jname header=0|wc -l|awk '{print$1}')
-                for I in $(seq 1 $N); do
-                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n $I|head -n 1)
+                for I in $(seq 1 "$N"); do
+                        STATE=$(env NOCOLOR=1 cbsd jls display=status header=0|tail -n "$I"|head -n 1)
                         if [ "$STATE" == "On" ]; then
-                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n $I|head -n 1)
+                            JAIL=$(env NOCOLOR=1 cbsd jls display=jname header=0|tail -n "$I"|head -n 1)
                             if [ "$JAILS" == "" ]; then
                                 JAILS=$JAIL
                             else
@@ -119,11 +119,11 @@ _cbsd () {
                             fi
                         fi
                 done
-                COMPREPLY=( $( compgen -W "$JAILS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$JAILS" -- "$cur" ) )
                 ;;
             jremove)
                 JAILS=$(env NOCOLOR=1 cbsd jls display=jname header=0|tr -s '\n' ' ')
-                COMPREPLY=( $( compgen -W "$JAILS" -- $cur ) )
+                COMPREPLY=( $( compgen -W "$JAILS" -- "$cur" ) )
                 ;;
             *)
                 return 0
