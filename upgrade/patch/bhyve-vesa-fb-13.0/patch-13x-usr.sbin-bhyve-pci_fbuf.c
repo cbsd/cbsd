effccfacb2f95883e02f4ca426871282fe344114
olevole <olevole@olevole.ru>
2021-12-07 14:40:34 +0300

    sync patch with latest

--- upgrade/patch/bhyve-vesa-fb-13.0/patch-13x-usr.sbin-bhyve-pci_fbuf.c
+++ upgrade/patch/bhyve-vesa-fb-13.0/patch-13x-usr.sbin-bhyve-pci_fbuf.c
@@ -1,6 +1,6 @@
---- pci_fbuf.c.orig	2021-01-23 00:09:48.409575000 +0300
-+++ pci_fbuf.c	2021-01-23 00:09:24.614356000 +0300
-@@ -368,7 +368,7 @@
+--- pci_fbuf.c.orig	2021-11-22 11:29:36.212615000 +0300
++++ pci_fbuf.c	2021-12-07 10:43:12.005093000 +0300
+@@ -390,7 +390,7 @@
  	/* initialize config space */
  	pci_set_cfgdata16(pi, PCIR_DEVICE, 0x40FB);
  	pci_set_cfgdata16(pi, PCIR_VENDOR, 0xFB5D);
@@ -8,4 +8,4 @@
 +	pci_set_cfgdata8(pi, PCIR_CLASS, PCIS_DISPLAY_OTHER);
  	pci_set_cfgdata8(pi, PCIR_SUBCLASS, PCIS_DISPLAY_VGA);
  
- 	error = pci_emul_alloc_bar(pi, 0, PCIBAR_MEM32, DMEMSZ);
+ 	sc->fb_base = vm_create_devmem(
