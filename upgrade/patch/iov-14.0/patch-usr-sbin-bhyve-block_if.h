12c68f08417553c7032d11b5d56cba408af448cb
olevole <olevole@olevole.ru>
2022-06-02 14:54:16 +0300

    test

--- upgrade/patch/iov-14.0/patch-usr-sbin-bhyve-block_if.h
+++ upgrade/patch/iov-14.0/patch-usr-sbin-bhyve-block_if.h
@@ -1,5 +1,5 @@
---- block_if.h.orig	2021-11-22 11:04:15.639530000 +0300
-+++ block_if.h	2021-11-22 11:16:43.345431000 +0300
+--- block_if.h.orig	2022-06-01 12:19:18.214744000 +0300
++++ block_if.h	2022-06-01 12:20:12.163589000 +0300
 @@ -50,7 +50,7 @@
   * a single request.  BLOCKIF_RING_MAX is the maxmimum number of
   * pending requests that can be queued.
