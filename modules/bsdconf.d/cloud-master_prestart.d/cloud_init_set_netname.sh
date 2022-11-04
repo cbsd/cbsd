b898762debca6b37c88d532b0d125507c5aa62ab
olevole <olevole@olevole.ru>
2022-11-05 00:05:04 +0300

    firestarter tpl: wip

--- modules/bsdconf.d/cloud-master_prestart.d/cloud_init_set_netname.sh
+++ modules/bsdconf.d/cloud-master_prestart.d/cloud_init_set_netname.sh
@@ -21,9 +21,15 @@ for i in $( cbsd bpcibus jname=${jname} mode=get device_name=virtio-net ); do
 	[ -z "${iface_index}" ] && err 0 "cloud_init_set_netname.sh helper for $jname: unable to get iface index, skipp"
 	#todo: ci_interface_name ( default_ci_interface_name )
 	iface_name="enp0s${iface_index}"
-	#echo "${seq} $iface_name"
-#       ${SED_CMD} -i '' -e "s/name:.*\$/name: ${iface_name}/g"  -e "s/mac_address:.*\$/mac_address: ${nic_hwaddr0}/g"  ${NETWORK_CONFIG}
-	${SED_CMD} -i '' -e "s/name: eth${seq}*\$/name: ${iface_name}/g"  -e "s/mac_address:.*\$/mac_address: ${nic_hwaddr0}/g"  ${NETWORK_CONFIG}
+
+	# test for fire init
+	${HEAD_CMD} -1 ${NETWORK_CONFIG} | ${GREP_CMD} -q "/bin/sh" > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		# firestarter template
+		${SED_CMD} -i '' -e "s/ eth${seq}/ ${iface_name}/g" ${NETWORK_CONFIG}
+	else
+		${SED_CMD} -i '' -e "s/name: eth${seq}*\$/name: ${iface_name}/g"  -e "s/mac_address:.*\$/mac_address: ${nic_hwaddr0}/g"  ${NETWORK_CONFIG}
+	fi
 	seq=$(( seq + 1 ))
 done
 
