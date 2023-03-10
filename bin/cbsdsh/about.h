a22b58e9c5c4f3996cdca2c824320c591515060e
olevole <olevole@olevole.ru>
2023-03-11 00:16:16 +0300

    bump version

--- bin/cbsdsh/about.h
+++ bin/cbsdsh/about.h
@@ -1,1 +1,1 @@
-#define VERSION "13.2.0d"
+#define VERSION "13.1.25"
--- tests/cbsd_jcreate_test.sh
+++ tests/cbsd_jcreate_test.sh
@@ -30,7 +30,7 @@ testFreeBSDVersion() {
 # Test authorized_keys
 testAuthorizedKeys() {
 	cp ~cbsd/.ssh/authorized_keys "${dir}"/ || exit 1
-	cbsd jcreate jname="${jname}" vnet=1 ip4_addr="212.212.212.214/30" ci_gw4="212.212.212.213" ci_user_pubkey="authorized_keys" runasap=1
+	cbsd jcreate jname="${jname}" vnet=1 ip4_addr="212.212.212.214/30" ci_gw4="212.212.212.213" ci_user_pubkey="authorized_keys" runasap=1 interface=em0
 	K1=$(head -n1 "${dir}"/authorized_keys)
 	K2=$(head -n1 ~cbsd/jails-data/"${jname}"-data/root/.ssh/authorized_keys)
 	assertNotNull "Empty orig authkey string" "${K1}"
@@ -40,11 +40,13 @@ testAuthorizedKeys() {
 
 # check for sysrc
 test_sysrc() {
-	cbsd jcreate jname="${jname}" vnet=1 sysrc="ifconfig_eth0+='mtu 1450' inetd_enable=YES" runasap=1
+	cbsd jcreate jname="${jname}" vnet=1 sysrc="ifconfig_eth0+='mtu 1450' inetd_enable=YES" runasap=1 interface=em0
 	. ~cbsd/jails-data/"${jname}"-data/etc/rc.conf
 	# get last world in ifconfig, should be 1450
 	last=$(echo "${ifconfig_eth0}" | grep -o '[^ ]\+$')
+	echo "last = [${last}]"
 	assertEquals "sysrc+= not valid" "${last}" "1450"
+	#assertSame "sysrc+= not valid" "${last}" "1450"
 }
 
 . shunit2
